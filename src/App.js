ad467286020a15c252b43864289a5fc961a0a527
warty-warthog <wartywarthog7@gmail.com>
2020-07-14 22:12:22 +0000

    broken the homepage into smaller components and styled MenuItem component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Homepage from './components/Homepage';
+import Homepage from './pages/homepage/Homepage';
 
 const App = () => {
   return (
