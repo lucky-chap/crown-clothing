fe2e14bf63261b47f21bfd9fc6592c9723825994
warty-warthog <wartywarthog7@gmail.com>
2020-07-14 21:03:06 +0000

    created project with CRA and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './components/Homepage';
+
+const App = () => {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
